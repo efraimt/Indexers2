cd8245dae75d9afa563ea9a1aae6b47f734dd767
efraimt <efraimt@gmail.com>
2021-08-16 19:51:25 +0300

    Add project files.

---
+++ Indexers2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Indexers2
+{
+    public partial class Form1 : Form
+    {
+        StudentManagament students = new StudentManagament();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //var student = students.GetStudentById(int.Parse(textBox1.Text));
+            var student = students[int.Parse(textBox1.Text)];
+            
+            /*
+             כאן מופיע קוד שמציג את הסטודנט אם מצאנו כזה סטודנט
+             */
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var student = students[textBox5.Text];
+            var student2 = students["משה"];
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int grade = 85;
+            Student[] studendsArray = students[grade,29];
+            
+        }
+    }
+}
---
+++ Indexers2/Indexers.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers2
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Grade { get; set; }
+    }
+
+    public class StudentManagament
+    {
+        Student[] students = new Student[1000];
+        public int Count { get; set; }
+
+        public Student? GetStudentById(int id)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (students[i].Id == id) return students[i];
+
+            }
+            return null;
+        }
+
+        public Student? GetStudentByName(string name)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (students[i].Name == name) return students[i];
+
+            }
+            return null;
+        }
+
+        //public Student this[int i] => GetStudentById(i);
+        public Student this[string name]
+        {
+            get
+            {
+                return GetStudentByName(name);
+            }
+        }
+
+        public Student[] this[int grade, int age]
+        {
+            get
+            {
+                Student[] bestStudents = new Student[1000];
+                foreach (var student in students)
+                {
+                    if (student.Grade > grade)
+                        bestStudents[i] = student;
+                }
+                return bestStudents;
+            }
+        }
+
+
+    }
+
+}
